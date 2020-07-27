f1e3ed7aea5f36529a281ce75918795760d94fc3
MatrixPanda <zehan11s@hotmail.com>
2020-07-26 23:10:22 -0400

    WIP

--- student_profiles/src/App.vue
+++ student_profiles/src/App.vue
@@ -27,12 +27,16 @@ export default {
 </script>
 
 <style>
-/* #app {
+:root {
+    --primary-color: #D0D0D0;
+}
+
+#app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-} */
+  /* text-align: center; */
+  /* color: #2c3e50; */
+  /* margin-top: 60px; */
+}
 </style>
--- student_profiles/src/components/Profile.vue
+++ student_profiles/src/components/Profile.vue
@@ -1,19 +1,19 @@
 <template>
-  <div>
+  <div class="profile-container">
     <img v-bind:src='student.pic'>
-    <p>
-       <strong>{{ student.firstName }} {{ student.lastName }}</strong>
-       <br>
-       <small>Email: {{ student.email }}</small>
-       <br>
-       <small>Company: {{ student.company }}</small>
-       <br>
-       <small>Skill: {{ student.skill }}</small>
-       <br>
-       <small>Average: {{ getAvg(student.grades) }}%</small>
-       <br>
-
-    </p>
+    <div>
+        <h1> {{ student.firstName }} {{ student.lastName }} </h1>
+        <p>
+            <small>Email: {{ student.email }}</small>
+            <br>
+            <small>Company: {{ student.company }}</small>
+            <br>
+            <small>Skill: {{ student.skill }}</small>
+            <br>
+            <small>Average: {{ getAvg(student.grades) }}%</small>
+            <br>
+        </p>
+    </div>
   </div>
 </template>
 
@@ -31,5 +31,43 @@ export default {
 
 
 <style scoped>
-
+    img {
+        align-self: center;
+        border-radius: 100%;
+        border: 1px solid var(--primary-color);
+        width: 150px;
+        height: 150px;
+        margin: 2rem;
+    }
+    p {
+        margin-left: 1.3rem;
+    }
+    h1 {
+        text-transform: uppercase;
+    }
+    
+    .profile-container {
+        display: flex;
+        border-bottom: 1px solid var(--primary-color);
+        font-size: large;
+        /* position: relative; */
+        /* display: flex;
+        cursor: pointer;
+        z-index: 1;
+        background-color: #f5f5f5;
+        font-family: 'Roboto', sans-serif;
+        font-size: 1.3vw;
+        font-weight: bold;
+        border: none;
+        border-radius: 10rem;
+        width: 12em;
+        height: 4em;
+        text-align: left;
+        text-decoration: none;
+        margin-top: auto;
+        margin-bottom: auto;   
+        -webkit-transition-duration: 0.4s; 
+        transition-duration: 0.4s; 
+        box-shadow: -0.8rem 0.9rem 20px rgba(66, 66, 66, 0.3); */
+    }
 </style>
